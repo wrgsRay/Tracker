9d3136cabb7007d07b7333dab765c28418dde55f
Raymond <28991499+wrgsRay@users.noreply.github.com>
2018-12-05 17:13:13 -0800

    USPS class working for the first time

--- tracker.py
+++ tracker.py
@@ -2,37 +2,53 @@
 Python Version 3.7.1
 Author: wrgsRay
 """
-
-import requests
 from bs4 import BeautifulSoup as bs
+import requests
 
 
 class USPS:
     def __init__(self, awb):
         self.awb = awb
+        self.url = 'https://tools.usps.com/go/TrackConfirmAction?tLabels=' + self.awb
+        self.agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0'
+        self.headers = {'user-agent': self.agent}
+        self.html = ''
 
     def status_latest(self):
-        url = 'https://tools.usps.com/go/TrackConfirmAction?tLabels=' + self.awb
-        agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0'
-        headers = {'user-agent': agent}
-        res = requests.get(url, headers=headers)
+        global soup
+        res = requests.get(self.url, headers=self.headers)
         try:
             res.raise_for_status()
-            soup = bs(res.text, 'html.parser')
+            self.html = res.text
+            soup = bs(self.html, 'html.parser')
         except Exception as e:
             print(e)
-        title = soup.title.getText()
-        return title
+        current_status = soup.select_one('h2 > strong').string if (soup.select_one('h2 > strong') is not None) \
+            else 'Not Found'
+        return current_status
+
+    @staticmethod
+    def detailed_status():
+        result = list()
+        if soup.find(attrs={"class": "status_feed"}) is not None:
+            for string in soup.find(attrs={"class": "status_feed"}).stripped_strings:
+                result.append(string)
+            result_time = ' '.join(result[0].split())
+            result_status = result[1]
+            result_location = result[2]
+            return f'As of {result_time}, the package {result_status} and the last know location is {result_location}'
+        else:
+            return "Element not found"
 
 
 def main():
     package_1 = USPS('9405511699000799152259')
     print(package_1.awb)
     print(package_1.status_latest())
+    print(package_1.detailed_status())
 
 
 if __name__ == '__main__':
     main()
 else:
     pass
-# pass
