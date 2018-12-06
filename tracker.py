07cee9d7f7ed7eaf641c2d3d50eeec92ce1073f2
Raymond Wang <28991499+wrgsRay@users.noreply.github.com>
2018-12-05 22:56:29 -0800

    made a separate method for getting update

--- tracker.py
+++ tracker.py
@@ -1,5 +1,5 @@
 """
-Python Version 3.7.1
+Python Version 3
 Author: wrgsRay
 """
 from bs4 import BeautifulSoup as bs
@@ -13,9 +13,8 @@ class USPS:
         self.agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0'
         self.headers = {'user-agent': self.agent}
         self.html = ''
-        # TODO: do requests and soup in init
 
-    def status_latest(self):
+    def get_status(self):
         global soup
         res = requests.get(self.url, headers=self.headers)
         try:
@@ -24,6 +23,9 @@ class USPS:
             soup = bs(self.html, 'html.parser')
         except Exception as e:
             print(e)
+
+    @staticmethod
+    def status_latest():
         current_status = soup.select_one('h2 > strong').string if (soup.select_one('h2 > strong') is not None) \
             else 'Not Found'
         return current_status
@@ -46,6 +48,7 @@ class USPS:
 def main():
     package_1 = USPS('9405511699000799152259')
     print(package_1.awb)
+    package_1.get_status()
     print(package_1.status_latest())
     print(package_1.detailed_status())
 
